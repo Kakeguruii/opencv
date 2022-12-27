2b5b04bb1dd43a0faee31d71523ba29381a81077
Elizaveta Malezh <milkasps66@gmail.com>
2022-12-27 16:09:57 +0300

    lab7

---
+++ DisplayWeb.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+using namespace std;
+
+int main() {
+Mat image;
+namedWindow("Display window");
+VideoCapture cap(0);
+if (!cap.isOpened()) {
+cout << "cannot open camera";
+}
+while (true) {
+cap >> image;
+imshow("Display window", image);
+//waitKey(25);
+char c = (char)waitKey(1);
+if( c == 27 ) break;
+}
+return 0;
+}
+
